cf554efd2b5e9d6e55c3c17627aad67966f7c269
Andrew Magdy <andrewmagdygad@gmail.com>
2023-04-29 05:45:33 +0300

    feat: add release script to staging environment

--- forge.config.ts
+++ forge.config.ts
@@ -1,10 +1,11 @@
+require("dotenv").config();
 import type { ForgeConfig } from "@electron-forge/shared-types";
 import { MakerSquirrel } from "@electron-forge/maker-squirrel";
 import { MakerZIP } from "@electron-forge/maker-zip";
 import { MakerDeb } from "@electron-forge/maker-deb";
 import { MakerRpm } from "@electron-forge/maker-rpm";
 import { WebpackPlugin } from "@electron-forge/plugin-webpack";
-
+import { PublisherGithub } from "@electron-forge/publisher-github";
 import { mainConfig } from "./webpack.main.config";
 import { rendererConfig } from "./webpack.renderer.config";
 
@@ -31,6 +32,13 @@ const config: ForgeConfig = {
                     },
                 ],
             },
+        }),
+    ],
+    publishers: [
+        new PublisherGithub({
+            repository: { owner: "vrapeutic", name: "OpenDesktopApp" },
+            authToken: process.env.GITHUB_TOKEN,
+            draft: true,
         }),
     ],
 };
