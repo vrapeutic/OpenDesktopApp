268a0fec77cd0a38bd74ccc2c86c9fd66b4f5d5b
Andrew Magdy <andrewmagdygad@gmail.com>
2023-05-23 03:21:08 +0300

    fix: remove ubuntu distribuation

--- forge.config.ts
+++ forge.config.ts
@@ -15,7 +15,7 @@ const config: ForgeConfig = {
     makers: [
         new MakerSquirrel({}),
         new MakerZIP({}, ["darwin"]),
-        // new MakerRpm({}),
+        new MakerRpm({}),
         new MakerDeb({}),
     ],
     plugins: [
