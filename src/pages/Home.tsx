3956d57d7ee22767c931f1829fa03f7bae41e2d4
Kholoud-fares <kholoudshaker79@gmail.com>
2023-05-31 10:34:52 +0300

    home with cards as UI

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 
 import React from 'react'
-import { Button, Card, Flex ,Text, CardHeader} from '@chakra-ui/react'
+import { Button, Card, Flex ,Text, CardHeader, Box, FormControl,FormLabel,Switch,Checkbox} from '@chakra-ui/react'
 import MMcard from '../theme/components/MMChart.tsx/MMcard'
 import SBDcard from '../theme/components/SBDchart.tsx/SBDcard'
 import { Subscriptions } from '../assets/icons/Subscriptions'
+import { Specialists } from '../assets/icons/Specialists'
+import Statiscscard from '../theme/components/Statiscscard'
 
 
 
@@ -23,6 +25,17 @@ export default function Home() {
           lineHeight="29px"
           letterSpacing="-0.01em"
           >Home</Text>
+      <FormControl display='flex' alignItems='center' position="absolute" left="400" top="133">
+      <FormLabel htmlFor='email-alerts' mb='0'>
+           My Centers' View
+      </FormLabel>
+      <Switch/>
+      <FormLabel htmlFor='email-alerts' mb='0'>
+           My Networks' View
+      </FormLabel>
+      <Checkbox defaultChecked>Show My Stats Only</Checkbox>
+      </FormControl>
+
       <Button
             position="absolute"
             left="1087px"
@@ -39,7 +52,7 @@ export default function Home() {
             lineHeight="16px"
             fontStyle="normal"
              >Create New widget</Button>
-       <Button
+       {/* <Button
             position="absolute"
             variant="outline"
             left="1262px"
@@ -58,12 +71,12 @@ export default function Home() {
             _hover={{
               background:"#FFFFFF",
             }}
-             >exporting CSV</Button>
+             >exporting CSV</Button> */}
           
       <MMcard />
       <SBDcard />
 
-      {/* <Card
+      <Card
           position="absolute"
           width="480px"
           height="186px"
@@ -90,18 +103,111 @@ export default function Home() {
              left="24px"
              top="76px"
              backgroundColor="#783EFD"
+             borderRadius="8px"
              boxShadow="0px 4px 16px rgba(128, 234, 255, 0.64)"
-             borderRaduis="8px">
-
+             >
+            <Box 
+                position="absolute"
+                width="40px"
+                height="40px"
+                backgroundColor="#FFFFFF"
+                left="16px"
+                top="18px"
+                borderRadius="8px">
             <Subscriptions 
+                         position="absolute"
                          color="#783EFD"
-                         backgroundColor="#FFFFFF"/>
+                         backgroundColor="#FFFFFF"
+                         left="8px"
+                         top="8px"
+                        />
+          </Box>
+
+            
+          <Text
+               position="absolute"
+               top="18px"
+               left="70px"
+               color="#FFFFFF"
+               fontFamily="Roboto"
+               fontStyle="normal"
+               lineHeight="16px"
+               fontSize="14px"
+               fontWeight="700"
+               textTransform="capitalize">Subscriptions</Text>
+        
+        <Text
+               position="absolute"
+               top="42px"
+               left="70px"
+               color="#FFFFFF"
+               fontFamily="Roboto"
+               fontStyle="normal"
+               lineHeight="19px"
+               fontSize="14px"
+               fontWeight="700"
+               textTransform="capitalize">6</Text>
 
         </Card>
-      </Flex>
-      
-          
-      </Card> */}
+
+        <Card
+             direction={{ base: 'column', sm: 'row' }}
+             overflow='hidden'
+             position="absolute"
+             width="205px"
+             height="78px"
+             left="251px"
+             top="76px"
+             backgroundColor="#00DEA3"
+             borderRadius="8px"
+             boxShadow="0px 4px 16px rgba(128, 234, 255, 0.64)"
+             >
+            <Box 
+                position="absolute"
+                width="40px"
+                height="40px"
+                backgroundColor="#FFFFFF"
+                left="16px"
+                top="18px"
+                borderRadius="8px">
+            <Specialists 
+                         position="absolute"
+                         color="#00DEA3"
+                         backgroundColor="#FFFFFF"
+                         left="8px"
+                         top="8px"
+                        />
+          </Box>
+
+            
+          <Text
+               position="absolute"
+               top="18px"
+               left="70px"
+               color="#FFFFFF"
+               fontFamily="Roboto"
+               fontStyle="normal"
+               lineHeight="16px"
+               fontSize="14px"
+               fontWeight="700"
+               textTransform="capitalize">Specialists</Text>
+        
+        <Text
+               position="absolute"
+               top="42px"
+               left="70px"
+               color="#FFFFFF"
+               fontFamily="Roboto"
+               fontStyle="normal"
+               lineHeight="19px"
+               fontSize="14px"
+               fontWeight="700"
+               textTransform="capitalize">12</Text>
+
+        </Card>
+      </Flex> 
+      </Card>
+      <Statiscscard />
     </Flex>
   )
 }
