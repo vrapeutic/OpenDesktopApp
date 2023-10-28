955d0f768265549ee644100a2d1d43b907a9e0aa
MostafalzohryGebhaly <mostafa.elzohary@Gebhaly.com>
2023-10-28 18:01:19 +0200

    Multi Step Form

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
+
 export const config = {
     apiURL:
         (window as any).envVars.API_URL ??
-        "http://vrapeutic-rails-production.eba-2nd4efne.eu-west-1.elasticbeanstalk.com",
+        "http://vrapeutic-api-production.eba-7rjfenj2.eu-west-1.elasticbeanstalk.com/api/v1",
     appEnv:
         ((window as any).envVars.APP_ENV as "dev" | "production" | "staging") ??
         "dev",
