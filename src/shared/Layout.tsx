460e3504f6796729f3259ae1794848120b612ac8
Andrew Magdy <andrewmagdygad@gmail.com>
2023-11-19 22:19:21 +0200

    fix: signup-flow

--- src/shared/Layout.tsx
+++ src/shared/Layout.tsx
@@ -1,12 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Flex } from '@chakra-ui/react';
 import Header from '@renderer/features/header/Header';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import Sidebar from './Sidebar/Sidebar';
 import Sideclose from './Sidebar/Sideclose';
 
 export default function Layout() {
   const [menuOpen, setMenuOpen] = useState(true);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    (async () => {
+      if (location.pathname === '/signup') return;
+
+      const token = await (window as any).electronAPI.getPassword('token');
+      console.log('token: ', token);
+      if (!token) {
+        navigate('/login');
+      }
+    })();
+  }, [location.pathname]);
 
   const MenuHandler = () => {
     setMenuOpen(!menuOpen);
