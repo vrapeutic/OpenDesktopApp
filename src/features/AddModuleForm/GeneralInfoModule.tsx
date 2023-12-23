e3d6e4f744c84e1ba84ec6692b56c7f0306df2b2
Mostafa Elzohry <98609624+mostafalzohry@users.noreply.github.com>
2023-12-23 17:08:16 +0200

    Add module for Admin

---
+++ src/features/AddModuleForm/GeneralInfoModule.tsx
@@ -0,0 +1,302 @@
+import { useForm } from 'react-hook-form';
+import joi from 'joi';
+import {
+  Button,
+  Text,
+  Box,
+  FormLabel,
+  Grid,
+  GridItem,
+  Input,
+  Flex,
+} from '@chakra-ui/react';
+import { joiResolver } from '@hookform/resolvers/joi';
+import Progressbar from '../../theme/components/ProgressBarAddCenter';
+import { TherapyFormProps } from '../AddCenterForm/therapyFormInterface';
+import Select from 'react-select';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { config } from '@renderer/config';
+import makeAnimated from 'react-select/animated';
+import ProgressBarAddModule from '@renderer/theme/components/ProgressBarAddModule';
+
+const GeneralInfoModule: React.FC<TherapyFormProps> = ({
+  onSubmit,
+  nextHandler,
+  backHandler,
+  sliding,
+}) => {
+  const schema = joi.object({
+    Name: joi.string().min(3).max(30).required().label('Name'),
+
+    Type: joi.string().required(),
+    // Email: joi
+    //   .string()
+    //   .email({ tlds: { allow: false } })
+    //   .required(),
+    Skills: joi.string().min(3).max(30).required().label('Skills'),
+    From: joi.number().required(),
+    To: joi.number().required(),
+    specializationschema: joi.array().required().label('specializationschema'),
+
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm({
+    resolver: joiResolver(schema),
+    mode: 'onTouched',
+  });
+
+  const FormonSubmit = (data: {
+    Name: string;
+    Type: string;
+    Email: string;
+    // Skills: string;
+    From: number;
+    To:number;
+    specializationschema: any[];
+
+  }) => {
+    onSubmit(data);
+    nextHandler();
+  };
+
+
+  const [specialistslist, setspecialistslist] = useState([]);
+
+  useEffect(() => {
+    getSpecialists();
+  }, []);
+
+  const getSpecialists = async () => {
+    try {
+      const response = await axios.get(`${config.apiURL}/api/v1/targeted_skills`);
+      setspecialistslist(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+  const animatedComponents = makeAnimated();
+
+  const handleSpecializations = (options: any) => {
+    setValue('specializationschema', [...options]);
+  };
+  const specialties = specialistslist.map((speciality) => ({
+    id: speciality.id,
+    label: speciality.name,
+    value: speciality.id,
+  }));
+
+  return (
+    <Box
+      bg="#FFFFFF"
+      borderRadius="10px"
+      m="5.875em 2.625em 5.875em 2.375em"
+      as="form"
+      onSubmit={handleSubmit(FormonSubmit)}
+    >
+      <ProgressBarAddModule index={0} />
+      <Grid
+        m="2.625em 1.5em 0em 1.5em"
+        templateColumns="repeat(2, 1fr)"
+        gap="0em 1.5625em"
+      >
+        <GridItem>
+          <FormLabel
+            display="inline"
+            m="0em"
+            letterSpacing="0.256px"
+            color="#15134B"
+          >
+            Name
+          </FormLabel>
+
+          <Input
+            {...register('Name')}
+            id="Name"
+            borderColor="#4965CA"
+            border="2px solid #E8E8E8"
+            _hover={{ border: '1px solid #4965CA' }}
+            boxShadow="0px 0px 4px 0px rgba(57, 97, 251, 0.30)"
+            type="text"
+            mt="0.75em"
+            mb="1em"
+            borderRadius="8px"
+          />
+          {errors.Name && (
+            <Text color="red.500">{errors.Name.message as string}</Text>
+          )}
+        </GridItem>
+        <GridItem>
+          <FormLabel
+            display="inline"
+            m="0em"
+            letterSpacing="0.256px"
+            color="#15134B"
+          >
+            Type
+          </FormLabel>
+
+          <Input
+            {...register('Type')}
+            id="Type"
+            borderColor="#4965CA"
+            border="2px solid #E8E8E8"
+            _hover={{ border: '1px solid #4965CA' }}
+            boxShadow="0px 0px 4px 0px rgba(57, 97, 251, 0.30)"
+            type="text"
+            mt="0.75em"
+            mb="1em"
+            borderRadius="8px"
+          />
+          {errors.Type && (
+            <Text color="red.500">{errors.Type.message as string}</Text>
+          )}
+        </GridItem>
+        <GridItem>
+          <FormLabel
+            display="inline"
+            mb="1em"
+            letterSpacing="0.256px"
+            color="#15134B"
+          >
+            Age Range
+          </FormLabel>
+          <Grid gap={2} templateColumns="repeat(2, 1fr)">
+            <GridItem>
+              <FormLabel
+                display="inline"
+                m="0em"
+                letterSpacing="0.256px"
+                color="#15134B"
+                fontSize={12}
+              >
+                From
+              </FormLabel>
+
+              <Input
+                {...register('From')}
+                id="From"
+                borderColor="#4965CA"
+                border="2px solid #E8E8E8"
+                _hover={{ border: '1px solid #4965CA' }}
+                boxShadow="0px 0px 4px 0px rgba(57, 97, 251, 0.30)"
+                type="text"
+                mt="0.75em"
+                mb="1em"
+                borderRadius="8px"
+              />
+              {errors.From && (
+                <Text color="red.500">{errors.From.message as string}</Text>
+              )}
+            </GridItem>
+            <GridItem>
+              <FormLabel
+                display="inline"
+                m="0em"
+                letterSpacing="0.256px"
+                color="#15134B"
+                fontSize={12}
+              >
+                To
+              </FormLabel>
+
+              <Input
+                {...register('To')}
+                id="To"
+                borderColor="#4965CA"
+                border="2px solid #E8E8E8"
+                _hover={{ border: '1px solid #4965CA' }}
+                boxShadow="0px 0px 4px 0px rgba(57, 97, 251, 0.30)"
+                type="text"
+                mt="0.75em"
+                mb="1em"
+                borderRadius="8px"
+              />
+              {errors.To && (
+                <Text color="red.500">{errors.To.message as string}</Text>
+              )}
+            </GridItem>
+          </Grid>
+        </GridItem>
+        <GridItem>
+          <FormLabel
+            display="inline"
+            m="0em"
+            letterSpacing="0.256px"
+            color="#15134B"
+          >
+            Skills
+          </FormLabel>
+{/* 
+          <Input
+            {...register('Skills')}
+            id="Skills"
+            borderColor="#4965CA"
+            border="2px solid #E8E8E8"
+            _hover={{ border: '1px solid #4965CA' }}
+            boxShadow="0px 0px 4px 0px rgba(57, 97, 251, 0.30)"
+            type="text"
+            mt="0.75em"
+            mb="1em"
+            borderRadius="8px"
+          /> */}
+           <Select
+            {...register('specializationschema')}
+            closeMenuOnSelect={false}
+            components={animatedComponents}
+            isMulti
+            options={specialties}
+            id="specializationschema"
+            name="specializationschema"
+            onChange={handleSpecializations}
+          />
+          {errors.specializationschema && (
+            <Text color="red.500">{errors.specializationschema.message as string}</Text>
+          )}
+        </GridItem>
+      </Grid>
+      <Flex flexDirection="row-reverse">
+        <Button
+          type="submit"
+          bg="#4AA6CA"
+          borderRadius="0.75em"
+          w="13.375em"
+          h="3.375em"
+          mt="0em"
+          mr="1.5em"
+          mb="2em"
+          color="#FFFFFF"
+          fontSize="1.125em"
+          fontWeight="700"
+        >
+          Next
+        </Button>
+
+        {sliding === 1 ? null : (
+          <Button
+            onClick={backHandler}
+            bg="#F5F5F5"
+            borderRadius="0.75em"
+            w="13.375em"
+            h="3.375em"
+            mt="0em"
+            ml="1.5em"
+            mb="2em"
+            mr="auto"
+            color="#A0A0A0"
+            fontSize="1.125em"
+            fontWeight="700"
+          >
+            Back
+          </Button>
+        )}
+      </Flex>
+    </Box>
+  );
+};
+export default GeneralInfoModule;
--- src/pages/Subscriptions.tsx
+++ src/pages/Subscriptions.tsx
@@ -1,5 +1,379 @@
-import React from 'react';
+// import { Button } from '@chakra-ui/button';
+// import { Box, Flex, Grid, Text } from '@chakra-ui/layout';
+// import HeaderSpaceBetween from '../theme/components/HeaderSpaceBetween'
+// import { getMe } from '@renderer/cache';
+// import { useState } from 'react';
+// import GeneralInfoForm from '@renderer/features/AddCenterForm/GeneralInfoForm';
+// import SpecialtyForm from '@renderer/features/AddCenterForm/SpecialtyForm';
+// import EductionIInfoForm from '@renderer/features/AddCenterForm/EductionIInfoForm';
+// import ContactForm from '@renderer/features/AddCenterForm/ContactForm';
+// import { Table, Td, Th, Thead, Tr } from '@chakra-ui/table';
+// import { Tag, TagLabel } from '@chakra-ui/tag';
+// import { useNavigate } from '@reach/router';
 
-export default function Subscriptions() {
-  return <div>Subscriptions</div>;
+//  function Subscriptions() {
+//   const totalSteps = 5;
+//   const [sliding, setSliding] = useState(1);
+//   const [formData, setFormData] = useState({});
+//   const [centersList, setCentersList] = useState([]);
+//   const [showTable, setShowTable] = useState(true);
+
+//   const token = getMe()?.token;
+//   const headers = {
+//     Authorization: `Bearer ${token}`,
+//   };
+
+//   const handleFormSubmit = (data: any) => {
+//     setFormData({ ...formData, ...data });
+//     return { ...formData, ...data };
+//   };
+
+//   const nextHandler = () => {
+//     if (sliding < totalSteps) {
+//       setSliding(sliding + 1);
+//       setShowTable(false);
+//     }
+//   };
+
+//   const backHandler = () => {
+//     if (sliding > 1) {
+//       setSliding(sliding - 1);
+//       if (sliding === 2) {
+//         setSliding(sliding - 1);
+//         setShowTable(true);
+//       }
+//     }
+//   };
+
+//   const renderFormStep = () => {
+//     switch (sliding) {
+//       case 2:
+//         return (
+//           <GeneralInfoForm
+//             onSubmit={handleFormSubmit}
+//             nextHandler={nextHandler}
+//             backHandler={backHandler}
+//             sliding={sliding}
+//           />
+//         );
+//       case 3:
+//         return (
+//           <SpecialtyForm
+//             onSubmit={handleFormSubmit}
+//             nextHandler={nextHandler}
+//             backHandler={backHandler}
+//             sliding={sliding}
+//           />
+//         );
+//       case 4:
+//         return (
+//           <EductionIInfoForm
+//             onSubmit={handleFormSubmit}
+//             nextHandler={nextHandler}
+//             backHandler={backHandler}
+//             sliding={sliding}
+//           />
+//         );
+//       case 5:
+//         return (
+//           <ContactForm
+//             onSubmit={handleFormSubmit}
+//             nextHandler={nextHandler}
+//             backHandler={backHandler}
+//             sliding={sliding}
+//             formData={formData}
+//           />
+//         );
+//       default:
+//         return null;
+//     }
+//   };
+
+//   const renderTable = () => {
+//     return (
+//       <Table
+//         variant="simple"
+//         background="#FFFFFF"
+//         style={{ marginLeft: 10, marginRight: 10 }}
+//       >
+//         <Thead>
+//           <Tr>
+//             <Th> Name</Th>
+//             <Th>Specialties</Th>
+//             <Th>Kids</Th>
+//           </Tr>
+//         </Thead>
+//         <Tbody>
+//           {centersList?.map((center) => (
+//             <Tr
+//               key={center.id}
+//               onClick={() => handleCenterClick(center)}
+//               cursor={'pointer'}
+//             >
+//               <Td>
+//                 <Flex direction="row" gap={2}>
+//                   <Box
+//                     width={197}
+//                     height={197}
+//                     alignItems={'center'}
+//                     display={'flex'}
+//                   >
+//                     <img
+//                       src={center?.attributes?.logo?.url}
+//                       alt={center?.attributes?.name}
+//                     />
+//                   </Box>
+
+//                   <Text
+//                     style={{
+//                       display: 'flex',
+//                       justifyContent: 'center',
+//                       alignItems: 'center',
+//                     }}
+//                   >
+//                     {center?.attributes?.name}
+//                   </Text>
+//                 </Flex>
+//               </Td>
+//               <Td>
+//                 {center?.attributes?.specialties?.map((specialty) => (
+//                   <Tag key={specialty.id} size="sm" colorScheme="gray" mr={1}>
+//                     <TagLabel>{specialty?.name}</TagLabel>
+//                   </Tag>
+//                 ))}
+//               </Td>
+//               <Td>{center?.attributes?.children_count}</Td>
+//             </Tr>
+//           ))}
+//         </Tbody>
+//       </Table>
+//     );
+//   };
+
+//   const navigate = useNavigate();
+
+//   const handleCenterClick = (center: Center) => {
+//     console.log('Clicked Center Data:', center);
+//     navigate('/ViewCenter', { state: center });
+//   };
+
+//   const renderTableOrForm = () => {
+//     if (sliding === 1) {
+//       return (
+//         <>
+//           <Grid
+//             style={{
+//               display: 'flex',
+//               justifyContent: 'space-between',
+//               marginTop: sliding === 1 ? '0' : '55px',
+//             }}
+//           >
+//             <Text
+//               padding="12px 24px"
+//               borderRadius="8px"
+//               fontSize="14px"
+//               fontFamily="Roboto"
+//             >
+//               Therapy Centers
+//             </Text>
+
+//             <Button
+//               w="143px"
+//               h="40px"
+//               ml="24px"
+//               mt="55px"
+//               padding="12px 24px"
+//               bg="#F5B50E"
+//               borderRadius="8px"
+//               fontSize="14px"
+//               fontFamily="Roboto"
+//               onClick={() => nextHandler()}
+//               boxShadow="0px 2px 8px rgba(251, 203, 24, 0.24)"
+//             >
+//               Add Therapy
+//             </Button>
+//           </Grid>
+//           {showTable && renderTable()}
+//         </>
+//       );
+//     } else {
+//       return renderFormStep();
+//     }
+//   };
+
+//   return (
+//     <>
+//       {/* <Grid
+//         style={{
+//           display: 'flex',
+//           justifyContent: 'space-between',
+//           margin: '20px',
+//         }}
+//       >
+//         <Text
+//           padding="12px"
+//           borderRadius="8px"
+//           fontFamily="Graphik LCG"
+//           fontSize="29px"
+//           fontWeight="500"
+//           lineHeight="29px"
+//           letterSpacing="-0.01em"
+//         >
+//           therapeutic modules
+//         </Text>
+
+//         <Button
+//           w="143px"
+//           h="40px"
+
+//           padding="12px"
+//           bg="#F5B50E"
+//           borderRadius="8px"
+//           fontSize="14px"
+//           fontFamily="Roboto"
+//           boxShadow="0px 2px 8px rgba(251, 203, 24, 0.24)"
+//         >
+//           Add New Module
+//         </Button>
+//       </Grid> */}
+//       <HeaderSpaceBetween Title={"therapeutic modules"} ButtonText={"Add New Module"}/>
+
+//     </>
+//   );
+// }
+// export default Subscriptions
+
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { config } from '../config';
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Tag,
+  TagLabel,
+  Img,
+  Flex,
+  Text,
+  Box,
+  Button,
+  Grid,
+} from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
+import { getMe } from '../cache';
+import ContactForm from '../features/AddCenterForm/ContactForm';
+import EductionIInfoForm from '../features/AddCenterForm/EductionIInfoForm';
+import GeneralInfoForm from '../features/AddCenterForm/GeneralInfoForm';
+import SpecialtyForm from '../features/AddCenterForm/SpecialtyForm';
+import HeaderSpaceBetween from '../theme/components/HeaderSpaceBetween';
+import GeneralInfoModule from '@renderer/features/AddModuleForm/GeneralInfoModule';
+
+interface Center {
+  id: number;
+  attributes: {
+    name: string;
+    logo: {
+      url: string;
+    };
+    specialties: { id: number; name: string }[];
+    children_count: number;
+  };
 }
+
+const Subscriptions: React.FC = () => {
+  const totalSteps = 3;
+  const [sliding, setSliding] = useState(1);
+  const [formData, setFormData] = useState({});
+
+  const handleFormSubmit = (data: any) => {
+    setFormData({ ...formData, ...data });
+    return { ...formData, ...data };
+  };
+
+  const nextHandler = () => {
+    if (sliding < totalSteps) {
+      setSliding(sliding + 1);
+    }
+  };
+
+  const backHandler = () => {
+    if (sliding > 1) {
+      setSliding(sliding - 1);
+    }
+  };
+
+  const renderFormStep = () => {
+    switch (sliding) {
+      case 1:
+        return (
+          <HeaderSpaceBetween
+            Title={'therapeutic modules'}
+            ButtonText={'Add New Module'}
+            onClickFunction={nextHandler}
+          />
+        );
+      case 2:
+        return (
+          <GeneralInfoModule
+            onSubmit={handleFormSubmit}
+            nextHandler={nextHandler}
+            backHandler={backHandler}
+            sliding={sliding}
+          />
+        );
+      case 3:
+        return (
+          <SpecialtyForm
+            onSubmit={handleFormSubmit}
+            nextHandler={nextHandler}
+            backHandler={backHandler}
+            sliding={sliding}
+          />
+        );
+      case 4:
+        return (
+          <EductionIInfoForm
+            onSubmit={handleFormSubmit}
+            nextHandler={nextHandler}
+            backHandler={backHandler}
+            sliding={sliding}
+          />
+        );
+      case 5:
+        return (
+          <ContactForm
+            onSubmit={handleFormSubmit}
+            nextHandler={nextHandler}
+            backHandler={backHandler}
+            sliding={sliding}
+            formData={formData}
+          />
+        );
+      default:
+        return null;
+    }
+  };
+
+  // const renderTableOrForm = () => {
+  //   if (sliding === 1) {
+  //     return (
+  //       <>
+  //         <HeaderSpaceBetween
+  //           Title={'therapeutic modules'}
+  //           ButtonText={'Add New Module'}
+  //           onClickFunction={nextHandler}
+  //         />
+  //       </>
+  //     );
+  //   } else {
+  //     return renderFormStep();
+  //   }
+  // };
+
+  return <>{renderFormStep()}</>;
+};
+
+export default Subscriptions;
