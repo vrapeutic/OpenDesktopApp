5f59527ec35e35c1ccdab53ca15e1925a55b03ca
Mostafa Elzohry <98609624+mostafalzohry@users.noreply.github.com>
2024-04-14 20:00:33 +0200

    fix with add center

--- src/theme/components/ProgressBarAddCenter.tsx
+++ src/theme/components/ProgressBarAddCenter.tsx
@@ -1,3 +1,247 @@
+// import {
+//   Step,
+//   StepIcon,
+//   StepIndicator,
+//   StepSeparator,
+//   StepStatus,
+//   StepTitle,
+//   Stepper,
+//   Flex,
+//   Text,
+// } from '@chakra-ui/react';
+// import { ArrowDown } from '../../assets/icons/ArrowDown';
+
+// const ProgressBarAddCenter = (props: any) => {
+//   const steps = [
+//     { title: 'General info', icon: <ArrowDown /> },
+//     { title: 'Specialty', icon: <ArrowDown /> },
+//     { title: 'Education info', icon: <ArrowDown /> },
+//     { title: 'Contact', icon: <ArrowDown /> },
+//   ];
+
+//   return (
+//     <>
+//       <Stepper
+//         index={props.index}
+//         position="relative"
+//         top="27px"
+//         left="50%"
+//         transform="translateX(-50%)"
+//         w={{ base: "90%", md: "756px" }}
+//         h="101px"
+//         colorScheme="whatsapp"
+//         overflowX="hidden"
+//       >
+//         {steps.map((step, index) => (
+//           <Step key={index}>
+//             <Flex direction="column" justifyContent="center">
+//               <StepIndicator
+//                 top="32px"
+//                 left={{ base: "50%", md: "206px" }}
+//                 transform="translateX(-50%)"
+//                 w={{ base: "30%", md: "62px" }}
+//                 h="62px"
+//                 margin="5px"
+//               >
+//                 <StepStatus
+//                   complete={
+//                     <Flex
+//                       flexDirection="column"
+//                       alignItems="center"
+//                       pt="22px"
+//                       color="#00DEA3"
+//                     >
+//                       <StepIcon
+//                         bgColor="#00DEA3"
+//                         h="62px"
+//                         w="62px"
+//                         borderRadius="50%"
+//                         color="#FFFFFF"
+//                       />
+//                       <StepTitle>{step.title}</StepTitle>
+//                     </Flex>
+//                   }
+//                   incomplete={
+//                     <Flex
+//                       flexDirection="column"
+//                       alignItems="center"
+//                       style={{ marginTop: '45px' }}
+//                     >
+//                       {step.icon}{' '}
+//                       <StepTitle
+//                         style={{
+//                           width: '150px',
+//                           marginTop: '20px',
+//                           marginLeft: '80px',
+//                           color: '#A0A0A0',
+//                         }}
+//                       >
+//                         {step.title}
+//                       </StepTitle>
+//                     </Flex>
+//                   }
+//                   active={
+//                     <Flex
+//                       flexDirection="column"
+//                       alignItems="center"
+//                       style={{
+//                         backgroundColor: '#00DEA3',
+//                         height: '62px',
+//                         width: '62px',
+//                         borderRadius: '50%',
+//                         paddingTop: '22%',
+//                         color: '#00DEA3',
+//                       }}
+//                     >
+//                       {step.icon}{' '}
+//                       <StepTitle
+//                         style={{
+//                           marginTop: '35px',
+//                           marginLeft: '30px',
+//                           width: '120px',
+//                         }}
+//                       >
+//                         {step.title}
+//                       </StepTitle>
+//                     </Flex>
+//                   }
+//                 />
+//               </StepIndicator>
+//             </Flex>
+//             <StepSeparator />
+//           </Step>
+//         ))}
+//       </Stepper>
+//     </>
+//   );
+// };
+
+// export default ProgressBarAddCenter;
+
+
+
+// import {
+//   Step,
+//   StepIcon,
+//   StepIndicator,
+//   StepSeparator,
+//   StepStatus,
+//   StepTitle,
+//   Stepper,
+//   Flex,
+//   Text,
+// } from '@chakra-ui/react';
+// import { ArrowDown } from '../../assets/icons/ArrowDown';
+
+// const ProgressBarAddCenter = (props: any) => {
+//   const steps = [
+//     { title: 'General info', icon: <ArrowDown /> },
+//     { title: 'Specialty', icon: <ArrowDown /> },
+//     { title: 'Education info', icon: <ArrowDown /> },
+//     { title: 'Contact', icon: <ArrowDown /> },
+//   ];
+
+//   return (
+//     <>
+//       <Stepper
+//         index={props.index}
+//         position="relative"
+//         top="27px"
+//         left="50%"
+//         transform="translateX(-50%)"
+//         w={{ base: "90%", md: "756px" }}
+//         h="101px"
+//         colorScheme="whatsapp"
+//         overflowX="hidden"
+//       >
+//         {steps.map((step, index) => (
+//           <Step key={index}>
+//             <Flex direction="column" justifyContent="center">
+//               <StepIndicator
+//                 top="32px"
+//                 left={`calc(${index} * (100% / ${steps.length}))`}
+//                 transform="translateX(-50%)"
+//                 w={{ base: "30%", md: "62px" }}
+//                 h="62px"
+//                 margin="5px"
+//               >
+//                 <StepStatus
+//                   complete={
+//                     <Flex
+//                       flexDirection="column"
+//                       alignItems="center"
+//                       pt="22px"
+//                       color="#00DEA3"
+//                     >
+//                       <StepIcon
+//                         bgColor="#00DEA3"
+//                         h="62px"
+//                         w="62px"
+//                         borderRadius="50%"
+//                         color="#FFFFFF"
+//                       />
+//                       <StepTitle>{step.title}</StepTitle>
+//                     </Flex>
+//                   }
+//                   incomplete={
+//                     <Flex
+//                       flexDirection="column"
+//                       alignItems="center"
+//                       style={{ marginTop: '45px' }}
+//                     >
+//                       {step.icon}{' '}
+//                       <StepTitle
+//                         style={{
+//                           width: '150px',
+//                           marginTop: '20px',
+//                           marginLeft: '80px',
+//                           color: '#A0A0A0',
+//                         }}
+//                       >
+//                         {step.title}
+//                       </StepTitle>
+//                     </Flex>
+//                   }
+//                   active={
+//                     <Flex
+//                       flexDirection="column"
+//                       alignItems="center"
+//                       style={{
+//                         backgroundColor: '#00DEA3',
+//                         height: '62px',
+//                         width: '62px',
+//                         borderRadius: '50%',
+//                         paddingTop: '22%',
+//                         color: '#00DEA3',
+//                       }}
+//                     >
+//                       {step.icon}{' '}
+//                       <StepTitle
+//                         style={{
+//                           marginTop: '35px',
+//                           marginLeft: '30px',
+//                           width: '120px',
+//                         }}
+//                       >
+//                         {step.title}
+//                       </StepTitle>
+//                     </Flex>
+//                   }
+//                 />
+//               </StepIndicator>
+//             </Flex>
+//             {index !== steps.length - 1 && <StepSeparator />}
+//           </Step>
+//         ))}
+//       </Stepper>
+//     </>
+//   );
+// };
+
+// export default ProgressBarAddCenter;
+
+
+
 import {
   Step,
   StepIcon,
@@ -7,7 +251,6 @@ import {
   StepTitle,
   Stepper,
   Flex,
-  Text,
 } from '@chakra-ui/react';
 import { ArrowDown } from '../../assets/icons/ArrowDown';
 
@@ -19,50 +262,28 @@ const ProgressBarAddCenter = (props: any) => {
     { title: 'Contact', icon: <ArrowDown /> },
   ];
 
-  const activeStatus = {
-    backgroundColor: '#00DEA3',
-    height: '52px',
-    width: '52px',
-    borderRadius: '50%',
-    paddingTop: '22%',
-    color: '#00DEA3',
-  };
-
-  const title = {
-    marginTop: '35px',
-    marginLeft: '30px',
-    width: '120px',
-  };
-
-  const inCompleteStatus = {
-    marginTop: '45px',
-  };
-
-  const inCompleteTitle = {
-    width: '150px',
-    marginTop: '20px',
-    marginLeft: '80px',
-    color: '#A0A0A0',
-  };
-
   return (
     <>
       <Stepper
         index={props.index}
         position="relative"
         top="27px"
-        left="182px"
-        w="756px"
-        h="101px"
+        left="50%"
+        transform="translateX(-50%)"
+        w={{ base: "90vw", md: "756px" }} 
+        h="132px"
         colorScheme="whatsapp"
+        overflowX="hidden"
+        paddingLeft={"50px"}
       >
         {steps.map((step, index) => (
           <Step key={index}>
             <Flex direction="column" justifyContent="center">
               <StepIndicator
                 top="32px"
-                left="206px"
-                w="62px"
+                left={`calc(${index} * (100% / ${steps.length}))`}
+                transform="translateX(-50%)"
+                w={{ base: "30%", md: "62px" }}
                 h="62px"
                 margin="5px"
               >
@@ -81,19 +302,24 @@ const ProgressBarAddCenter = (props: any) => {
                         borderRadius="50%"
                         color="#FFFFFF"
                       />
-                      <StepTitle style={{ width: '100px', marginLeft: '20px' }}>
-                        {step.title}
-                      </StepTitle>
+                      <StepTitle>{step.title}</StepTitle>
                     </Flex>
                   }
                   incomplete={
                     <Flex
                       flexDirection="column"
                       alignItems="center"
-                      style={inCompleteStatus}
+                      style={{ marginTop: '45px' }}
                     >
                       {step.icon}{' '}
-                      <StepTitle style={inCompleteTitle}>
+                      <StepTitle
+                        style={{
+                          width: '150px',
+                          marginTop: '20px',
+                          marginLeft: '80px',
+                          color: '#A0A0A0',
+                        }}
+                      >
                         {step.title}
                       </StepTitle>
                     </Flex>
@@ -102,27 +328,31 @@ const ProgressBarAddCenter = (props: any) => {
                     <Flex
                       flexDirection="column"
                       alignItems="center"
-                      style={activeStatus}
+                      style={{
+                        backgroundColor: '#00DEA3',
+                        height: '62px',
+                        width: '62px',
+                        borderRadius: '50%',
+                        paddingTop: '22%',
+                        color: '#00DEA3',
+                      }}
                     >
                       {step.icon}{' '}
-                      <StepTitle style={title}>{step.title}</StepTitle>
+                      <StepTitle
+                        style={{
+                          marginTop: '35px',
+                          marginLeft: '30px',
+                          width: '120px',
+                        }}
+                      >
+                        {step.title}
+                      </StepTitle>
                     </Flex>
                   }
                 />
               </StepIndicator>
-
-              <Text
-                position="absolute"
-                marginTop="100px"
-                h="18px"
-                w="109px"
-                fontFamily="Graphik LCG"
-                fontWeight="400"
-                fontSize="18px"
-                lineHeight="18px"
-              ></Text>
             </Flex>
-            <StepSeparator />
+            {index !== steps.length - 1 && <StepSeparator />}
           </Step>
         ))}
       </Stepper>
@@ -131,3 +361,4 @@ const ProgressBarAddCenter = (props: any) => {
 };
 
 export default ProgressBarAddCenter;
+
