c4606624a02fdeca63991843b941040526a0bc19
yhyamohamed <yhyashehata1@gmail.com>
2024-05-19 16:04:38 +0300

    add constants file

---
+++ electron/constants.ts
@@ -0,0 +1,15 @@
+export const  EXPRESS_PORT =  3001
+
+export const  SOCKET_ALLOWED_EVENTS = {
+  MESSAGE: "message",
+  START_APP: "start-app",
+  START_SESSION: "start-session",
+}
+
+export const SERVER_LOGS_COLOR = "\x1b[36m%s\x1b[0m"
+
+export const START_APP_MESSAGE = "playModule"
+
+export const ANDROID_PACKAGE_NAME = "com.VRapeutic.RichiStandalone"
+
+export const MODULE_PACKAGE_KEY = 'modulePackageName'
