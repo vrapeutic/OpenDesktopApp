5b13d60d2e33934f80c603ab0ac6647d18b3debd
yhyamohamed <yhyashehata1@gmail.com>
2024-05-20 11:36:37 +0300

    update constants file

--- electron/constants.ts
+++ electron/constants.ts
@@ -4,12 +4,11 @@ export const  SOCKET_ALLOWED_EVENTS = {
   MESSAGE: "message",
   START_APP: "start-app",
   START_SESSION: "start-session",
+  PLAY_MODULE: "play-module",
 }
 
 export const SERVER_LOGS_COLOR = "\x1b[36m%s\x1b[0m"
 
-export const START_APP_MESSAGE = "playModule"
-
-export const ANDROID_PACKAGE_NAME = "com.VRapeutic.RichiStandalone"
+export const START_APP_MESSAGE = "PLAY_MODULE" // this has to match a key in SOCKET_ALLOWED_EVENTS
 
 export const MODULE_PACKAGE_KEY = 'modulePackageName'
