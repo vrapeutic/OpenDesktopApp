fdf9c5d284ce96f4c890bd49ee7a8b0d548fd3cb
yhyamohamed <yhyashehata1@gmail.com>
2024-05-20 11:37:11 +0300

    add dispatchPlayModuleMessaget o SocketManagerProvider

--- src/Context/SocketManagerProvider.tsx
+++ src/Context/SocketManagerProvider.tsx
@@ -1,61 +1,82 @@
-import React,{ createContext, useCallback, useContext, useEffect, useMemo, useRef } from 'react'
-import { io }  from 'socket.io-client';
+import React, {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef,
+} from 'react';
+import { io } from 'socket.io-client';
 
 import { EXPRESS_PORT } from '../../electron/constants';
 
 const URL = `http://localhost:${EXPRESS_PORT}`;
 
-const socket  = io(URL);
+const socket = io(URL);
 
-const SocketManagerContext = createContext(null)
+const SocketManagerContext = createContext(null);
 
+const SocketManagerProvider = ({ children }: { children: React.ReactNode }) => {
+  const emitMessage = useCallback(
+    (channel: string, message: string) => {
+      socket.emit(channel, message);
+    },
+    [socket]
+  );
 
-
- const SocketManagerProvider  = ({children}:{ children: React.ReactNode}) => {
-
-  const emitMessage = useCallback((channel:string,message: string) => {
-    socket.emit(channel, message)
-  },[socket])
-
-  const checkIfServiceExists = useCallback(async(deviceId: string) => {
-    const deviseIsConnected = await (window as any).electronAPI.checkServiceExistence(deviceId)
- console.log('deviseIsConnected',!!deviseIsConnected);
-  
-    
-    return  !!deviseIsConnected
-  },[socket]);
-
+  const dispatchPlayModuleMessage = useCallback(
+    (channel: string, message: string, ...rest: any[]) => {
+      socket.emit(channel, { message, setting: rest });
+    },
+    [socket]
+  );
+  const checkIfServiceExists = useCallback(
+    async (deviceId: string) => {
+      const deviseIsConnected = await (
+        window as any
+      ).electronAPI.checkServiceExistence(deviceId);
+      return !!deviseIsConnected;
+    },
+    [socket]
+  );
 
   useEffect(() => {
     socket.connect();
 
-    socket.on("connect", () => {
-      console.log("Connected to Socket.IO server");
+    socket.on('connect', () => {
+      console.log('Connected to Socket.IO server');
     });
 
     return () => {
-      socket.disconnect()
-    }
-  }, [])
+      socket.disconnect();
+    };
+  }, []);
 
+  const contextValue = useMemo(
+    () => ({
+      emitMessage,
+      checkIfServiceExists,
+      dispatchPlayModuleMessage,
+    }),
+    [socket]
+  );
 
-  const contextValue = useMemo(() => ({ 
-    emitMessage,
-    checkIfServiceExists
-  }), [socket])
-
-
-  return <SocketManagerContext.Provider value={contextValue}>{children}</SocketManagerContext.Provider>
-}
+  return (
+    <SocketManagerContext.Provider value={contextValue}>
+      {children}
+    </SocketManagerContext.Provider>
+  );
+};
 
 const useSocketManager = () => {
   const context = useContext(SocketManagerContext);
   if (!context) {
-    throw new Error('useSocketManagerContext must be used within an SocketManagerProvider');
+    throw new Error(
+      'useSocketManagerContext must be used within an SocketManagerProvider'
+    );
   }
   return context;
 };
 
-
-export default  useSocketManager;
-export  { SocketManagerProvider }
+export default useSocketManager;
+export { SocketManagerProvider };
