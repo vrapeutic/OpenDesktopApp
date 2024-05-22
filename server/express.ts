b5f0f0b5f0747db1154c28e3631129c1895cdf72
yhyamohamed <yhyashehata1@gmail.com>
2024-05-22 12:23:19 +0300

    add upload scv logic

--- server/express.ts
+++ server/express.ts
@@ -2,13 +2,14 @@ import {
   SERVER_LOGS_COLOR,
   SOCKET_ALLOWED_EVENTS,
 } from '../electron/constants';
-import * as  express from 'express';
+import * as express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
+import { writeFile } from 'fs';
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server,{
+const io = new Server(server, {
   cors: {
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST'],
@@ -33,10 +34,17 @@ app.post('/start-app', (req, res) => {
 io.on('connection', (socket) => {
   console.log(SERVER_LOGS_COLOR, 'a client connected');
 
+  socket.on('generateCsv', (file, callback) => {
+    console.log(file);
+    writeFile('../src/public/csvs/received_data.csv', file, (err) => {
+      callback({ message: err ? 'failure' : 'success' });
+    });
+  });
+
   socket.onAny((eventName, ...args) => {
-    const event = SOCKET_ALLOWED_EVENTS[eventName as keyof typeof SOCKET_ALLOWED_EVENTS]
-    if (!event)
-      throw new Error('invalid event name: ' + eventName);
+    const event =
+      SOCKET_ALLOWED_EVENTS[eventName as keyof typeof SOCKET_ALLOWED_EVENTS];
+    if (!event) throw new Error('invalid event name: ' + eventName);
 
     console.log(
       SERVER_LOGS_COLOR,
