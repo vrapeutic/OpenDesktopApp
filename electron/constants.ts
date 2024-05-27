22f4e457a28301c9c8a7fbed71f000362e068220
yhyamohamed <yhyashehata1@gmail.com>
2024-05-27 12:07:19 +0300

    clean up constants file

--- electron/constants.ts
+++ electron/constants.ts
@@ -1,14 +1,15 @@
 export const EXPRESS_PORT = 3001;
 
+export const RECEIVED_MESSAGES = {
+  generateSessionReport: 'generateSessionReport',
+};
+
 export const SOCKET_ALLOWED_EVENTS = {
   PLAY_MODULE: 'playModule',
   END_SESSION: 'endSession',
   GENERATE_SESSION_REPORT: 'generateSessionReport',
   MODULE_NOT_FOUND: 'moduleNotFound',
-};
-
-export const OUTGOING_ONLY_MESSAGES = {
-  GENERATE_SESSION_REPORT: 'generateSessionReport',
+  ...RECEIVED_MESSAGES,
 };
 
 export const SERVER_LOGS_COLOR = '\x1b[36m%s\x1b[0m';
@@ -16,9 +17,8 @@ export const YELLOW_SERVER_LOGS_COLOR = '\x1b[33m%s\x1b[0m';
 
 export const START_APP_MESSAGE = 'PLAY_MODULE'; // this has to match a key in SOCKET_ALLOWED_EVENTS
 export const END_SESSION_MESSAGE = 'END_SESSION';
-export const GENERATE_SESSION_REPORT = 'GENERATE_SESSION_REPORT';
 export const MODULE_NOT_FOUND_ERROR_MESSAGE = 'MODULE_NOT_FOUND';
 
 export const MODULE_PACKAGE_KEY = 'modulePackageName';
 
-export const REPORT_FILE_SAVE_PATH = 'Desktop/sessions_reports';
+export const REPORT_FILE_SAVE_PATH = 'Desktop/VRapeuticSessions';
