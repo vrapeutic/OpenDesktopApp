a7c3c67c9e1798b81a85458dd2dfac50ca774b50
yhyamohamed <yhyashehata1@gmail.com>
2024-05-27 12:48:23 +0300

    update moduleNotFound key in constants

--- electron/constants.ts
+++ electron/constants.ts
@@ -17,7 +17,7 @@ export const YELLOW_SERVER_LOGS_COLOR = '\x1b[33m%s\x1b[0m';
 
 export const START_APP_MESSAGE = 'PLAY_MODULE'; // this has to match a key in SOCKET_ALLOWED_EVENTS
 export const END_SESSION_MESSAGE = 'END_SESSION';
-export const MODULE_NOT_FOUND_ERROR_MESSAGE = 'MODULE_NOT_FOUND';
+export const MODULE_NOT_FOUND_ERROR_MESSAGE = 'moduleNotFound';
 
 export const MODULE_PACKAGE_KEY = 'modulePackageName';
 
