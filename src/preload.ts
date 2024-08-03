b2200cc8c58bc8bc93178218475be8c77c037507
EsraaIsmaiel1 <esraa.esmaiel@gmail.com>
2024-08-03 22:42:37 +0300

    added read file and files in path functions

--- src/preload.ts
+++ src/preload.ts
@@ -18,3 +18,9 @@ contextBridge.exposeInMainWorld('electronAPI', {
     ipcRenderer.invoke('checkServiceExistence', [deviceId]),
   checkNetworkConnection: () => ipcRenderer.invoke('checkNetworkConnection'),
 });
+
+contextBridge.exposeInMainWorld('electron', {
+  listFiles: (directoryPath: any) =>
+    ipcRenderer.invoke('list-files', directoryPath),
+  readFile: (filePath: any) => ipcRenderer.invoke('read-file', filePath),
+});
