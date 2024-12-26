02dbe0c4d88493316b12a885ee524c1002393548
EsraaIsmaiel1 <esraa.esmaiel@gmail.com>
2024-12-27 00:55:54 +0200

    add i18n support to NotFound component and update text translations

--- src/pages/home/components/Home.tsx
+++ src/pages/home/components/Home.tsx
@@ -380,10 +380,12 @@ export default function Home() {
 }
 
 export function NotFound() {
+  const { t, i18n } = useTranslation();
+
   return (
     <Box textAlign="center" py={10} px={6}>
       <Heading fontSize="2rem" mt={3} mb={2}>
-        You don't have center yet
+        {t('noCenter')}
       </Heading>
 
       <Link as={ReachLink} to={'/Therapycenters'}>
@@ -393,7 +395,7 @@ export function NotFound() {
           color="white"
           variant="solid"
         >
-          Go to Centers
+          {t('goToCenter')}
         </Button>
       </Link>
     </Box>
