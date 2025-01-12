264175538eb82aabc0d287541f5dd5256e81030f
manar Ramzi <manar.ali@brightcreations.com>
2025-01-13 01:21:02 +0200

    -prodaction stage

--- src/pages/SignUp/OTP.tsx
+++ src/pages/SignUp/OTP.tsx
@@ -63,10 +63,8 @@ export default function OTP() {
     const handleGetRequest = () => {
       setContextOtp(otp);
       fetch(
-        
-
-
-        'https://vrapeutic-api-production.eba-7rjfenj2.eu-west-1.elasticbeanstalk.com/api/v1/admins/software_modules',
+      
+        'https://vrapeutic-unicef.eu-west-1.elasticbeanstalk.com/api/v1/admins/software_modules',
         {
           headers: {
             otp: `${otp}`,
